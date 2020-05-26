e0e1a44fd1adc95fa31384fd282ed75de5d8f895
lyd <870502544@.com>
2020-05-26 23:17:30 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="home">
+		<image class="home-logo" src="/static/images/home_logo.png"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	.home{
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		&-logo{
+			width: 100%;
+			height: 100%;
+		}
+	}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,15 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/index/index"],{
+
+/***/ 15:
+/*!***************************************************************************!*\
+  !*** C:/Users/mi/Desktop/hui-zi/main.js?{"page":"pages%2Findex%2Findex"} ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/main.js):\nError: ENOENT: no such file or directory, open 'C:\\Users\\mi\\Desktop\\hui-zi\\main.js'");
+
+/***/ })
+
+},[[15,"common/runtime"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/index/index.js.map
